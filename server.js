d2fa6c6a745d5ca37d3e4ae95211af8f33cee9a5
rbtkoz <kozovski.a@gmail.com>
2016-11-02 16:45:51 -0400

    heroku

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-/**
- * Created by alex.kozovski on 11/2/16.
- */
 
 var express = require('express');
 var app = express();
-
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/build/'));
 app.listen(process.env.PORT || 8080);
